ca899bf05c6a776cb817781f90850e1e1d02b53f
jadolinic <jadolinic@gmail.com>
2023-04-08 18:54:38 +0200

    adding geojson, markers, binding popups

--- src/App.js
+++ src/App.js
@@ -1,43 +1,79 @@
-import './App.css';
-import React, { useEffect, useRef } from 'react';
-import { MapContainer, TileLayer, Marker, Popup, useMap, } from 'react-leaflet';
+import { Component } from 'react';
+import React from 'react';
+import { MapContainer, TileLayer, GeoJSON, } from 'react-leaflet';
 import 'leaflet/dist/leaflet.css';
-import L from 'leaflet';
+import './App.css'
+import L, { DivIcon, point, } from 'leaflet';
+import MarkerClusterGroup from 'react-leaflet-cluster';
+import lokacije from './objekti.json';
 
 
-delete L.Icon.Default.prototype._getIconUrl;
 
-L.Icon.Default.mergeOptions({
-  iconRetinaUrl: require('leaflet/dist/images/marker-icon-2x.png'),
-  iconUrl: require('leaflet/dist/images/marker-icon.png'),
-  shadowUrl: require('leaflet/dist/images/marker-shadow.png')
-});
+class App extends Component {
+  state = {};
 
-const location = [45.81603160595946, 15.978447002284671];
-const zoom = 13;
+  componentDidMount() {
+  }
+  
+  onEachLocation = (location, layer) => {
+    const nazivObjekta = location.properties.naziv_objekta;
+    const psBroj = location.properties.ps_br;
+    const eBroj = location.properties.e_br;
+    const tipObjekta = location.properties.tip_objekta;
+    const luckaKapetanija = location.properties.lucka_kapetanija;
+
+    layer.bindPopup(`Naziv objekt: <strong>${nazivObjekta}</strong></br>` +
+                    `PS Broj: <strong>${psBroj}</strong></br>` + 
+                    `E broj: <strong>${eBroj}</strong></br>` + 
+                    `Tip Objekta: <strong>${tipObjekta}</strong></br>` + 
+                    `Lučka Kapetanija: <strong>${luckaKapetanija}</strong>` );
+
+  }
 
 
-function App() {
+  render () {
+      delete L.Icon.Default.prototype._getIconUrl;
+      L.Icon.Default.mergeOptions({
+        iconRetinaUrl: require ('leaflet/dist/images/marker-icon-2x.png'),
+        iconUrl: require('leaflet/dist/images/marker-icon.png'),
+        shadowUrl: require ('leaflet/dist/images/marker-shadow.png'),
+      
+      }); 
 
-  const mapRef = useRef();
+      
+      const createCustomClusterIcon = (cluster) => {
+          return new DivIcon({
+            html: `<div class="cluster-icon">${cluster.getChildCount()}</div>`,
+            className: "custom-marker-cluster",
+            iconsSize: point(33, 33, true),
+      })
+      }
 
- useEffect( () => {
-  console.log(mapRef);
 
- }, [mapRef])
+  
+    return (
+      <>
+        
+        <MapContainer center={[43.86117730178092, 16.75881960042836]} zoom={8} scrollWheelZoom={true} >
+          <TileLayer 
+            attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors' 
+            url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png" 
+          />
+          
+          <MarkerClusterGroup
+              chunkedLoading
+              iconCreateFunction={createCustomClusterIcon}>
+            <GeoJSON 
+              data={lokacije.features}
+              onEachFeature={this.onEachLocation}
+             />
+          </MarkerClusterGroup>
 
-  return (
-    <div className="App">
-      <MapContainer ref={mapRef} center={location} zoom={zoom} scrollWheelZoom={true}>
-        <TileLayer attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors' url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png" />
-          <Marker position={location} >
-            <Popup>
-              Little things
-            </Popup>
-          </Marker>
-      </MapContainer>
-    </div>
-  );
+        </MapContainer>
+      </>
+    );
+  }
+   
 }
 
-export default App;
+  export default App;
